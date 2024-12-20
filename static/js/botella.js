fabc4dc9113162c1c799e44c138a426e72c858b4
Gabriel <gabriel.peral@iesdoctorbalmis.com>
2024-12-20 16:20:53 +0100

    caida y trasformacion del objeto

--- static/js/botella.js
+++ static/js/botella.js
@@ -2,13 +2,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const bottle = document.getElementById('waterBottle');
     let positionY = 0;
     let velocityY = 0;
-    let accelerationY = 0.1; 
+    let accelerationY = 0.05; 
     let rotation = 0;
     let lastScrollY = window.scrollY;
 
     function updateBottlePosition() {
         const maxY = window.innerHeight - bottle.offsetHeight;
         const scrollY = window.scrollY;
+        const totalHeight = document.body.scrollHeight - window.innerHeight;
 
         let scrollHaciaAbajo=false;
         if (scrollY > lastScrollY) {
@@ -35,6 +36,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
         rotation += 5;
         bottle.style.transform = `translateY(${positionY}px) rotate(${rotation}deg)`;
+
+        // Change bottle image based on scroll position
+        if (scrollY < totalHeight-totalHeight / 3) {
+            bottle.src = '../static/img/agua.png';
+        } else if (scrollY < totalHeight-totalHeight / 3 + (totalHeight-totalHeight / 3 )/2) {
+            bottle.src = '../static/img/bobina.png';
+        } else {
+            bottle.src = '../static/img/juguete-de-bebe.png';
+        }
 
         lastScrollY = scrollY;
         requestAnimationFrame(updateBottlePosition);
